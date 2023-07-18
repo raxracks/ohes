8272e95fa758a0c794b84d244511c545fb288660
raxracks <multisniperism@gmail.com>
2023-07-18 18:35:42 +1200

    remove dumb test code

--- kernel/src/main.rs
+++ kernel/src/main.rs
@@ -6,10 +6,8 @@ use core::arch::asm;
 extern crate alloc;
 extern crate font8x8;
 
-use alloc::vec::Vec;
 use buddy_system_allocator::LockedHeap;
-use font8x8::{UnicodeFonts, BASIC_FONTS};
-use klogger::{sprint, sprintln};
+use klogger::sprintln;
 use limine::{FramebufferRequest, MemmapRequest, MemoryMapEntryType};
 
 #[global_allocator]
@@ -19,94 +17,6 @@ static MEMMAP_REQUEST: MemmapRequest = MemmapRequest::new(0);
 
 #[no_mangle]
 unsafe extern "C" fn _start() -> ! {
-    if let Some(framebuffer_response) = FRAMEBUFFER_REQUEST.get_response().get() {
-        if framebuffer_response.framebuffer_count < 1 {
-            hcf();
-        }
-
-        let framebuffer = &framebuffer_response.framebuffers()[0];
-
-        let mut x_pos: usize = 0;
-        let mut y_pos: usize = 0;
-        let mut x_off: usize = 0;
-        let mut y_off: usize = 0;
-        if let Some(glyph) = BASIC_FONTS.get('h') {
-            for x in &glyph {
-                for bit in 0..8 {
-                    match *x & 1 << bit {
-                        0 => x_off += 1,
-                        _ => {
-                            for x in x_off..x_off + 1_usize {
-                                for y in y_off..y_off + 1_usize {
-                                    let pixel_offset =
-                                        (y_pos + y) * framebuffer.pitch as usize + (x_pos + x) * 4;
-                                    unsafe {
-                                        *(framebuffer
-                                            .address
-                                            .as_ptr()
-                                            .unwrap()
-                                            .offset(pixel_offset as isize)
-                                            as *mut u32) = 0xFFFFFFFF;
-                                    }
-                                }
-                            }
-                            x_off += 1;
-                        }
-                    }
-                }
-
-                if x_off > x_pos {
-                    x_pos = x_off;
-                }
-                x_off = 0;
-                y_off += 1;
-            }
-            if y_off > y_pos {
-                y_pos = y_off;
-            }
-            x_off = 0;
-            y_off = 0;
-        }
-
-        if let Some(glyph) = BASIC_FONTS.get('i') {
-            for x in &glyph {
-                for bit in 0..8 {
-                    match *x & 1 << bit {
-                        0 => x_off += 1,
-                        _ => {
-                            for x in x_off..x_off + 1_usize {
-                                for y in y_off..y_off + 1_usize {
-                                    let pixel_offset =
-                                        (y_pos + y) * framebuffer.pitch as usize + (x_pos + x) * 4;
-                                    unsafe {
-                                        *(framebuffer
-                                            .address
-                                            .as_ptr()
-                                            .unwrap()
-                                            .offset(pixel_offset as isize)
-                                            as *mut u32) = 0xFFFFFFFF;
-                                    }
-                                }
-                            }
-                            x_off += 1;
-                        }
-                    }
-                }
-
-                if x_off > x_pos {
-                    x_pos = x_off;
-                }
-                x_off = 0;
-                y_off += 1;
-            }
-            if y_off > y_pos {
-                y_pos = y_off;
-            }
-            x_off = 0;
-            y_off = 0;
-        }
-    }
-
     if let Some(memmap_response) = MEMMAP_REQUEST.get_response().get() {
         if memmap_response.entry_count < 1 {
             hcf();
